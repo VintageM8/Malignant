cf884e2fed397b3bb7b041194838b82512b9322f
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-11-09 17:34:02 -0500

    Add files via upload

---
+++ Common/MalignantSystem.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace Malignant.Common;
+
+public class MalignantSystem : ModSystem
+{
+    //bosses
+    public static bool downedViking;
+}
---
+++ Common/MethodHelper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+
+namespace Malignant.Common
+{
+    public static class MethodHelper
+    {
+        public static void SlowRotation(this ref float currentRotation, float targetAngle, float speed)
+        {
+            float actDirection = new Vector2((float)Math.Cos(currentRotation), (float)Math.Sin(currentRotation))
+                .ToRotation();
+            targetAngle = new Vector2((float)Math.Cos(targetAngle), (float)Math.Sin(targetAngle)).ToRotation();
+
+            int f;
+            //this makes f 1 or -1 to rotate the shorter distance
+            if (Math.Abs(actDirection - targetAngle) > Math.PI)
+            {
+                f = -1;
+            }
+            else
+            {
+                f = 1;
+            }
+
+            if (actDirection <= targetAngle + speed * 2 && actDirection >= targetAngle - speed * 2)
+            {
+                actDirection = targetAngle;
+            }
+            else if (actDirection <= targetAngle)
+            {
+                actDirection += speed * f;
+            }
+            else if (actDirection >= targetAngle)
+            {
+                actDirection -= speed * f;
+            }
+
+            actDirection = new Vector2((float)Math.Cos(actDirection), (float)Math.Sin(actDirection)).ToRotation();
+            currentRotation = actDirection;
+        }
+
+        public static void Move(this Projectile projectile, Vector2 vector, float speed, float turnResistance = 10f,
+            bool toPlayer = false)
+        {
+            Terraria.Player player = Main.player[projectile.owner];
+            Vector2 moveTo = toPlayer ? player.Center + vector : vector;
+            Vector2 move = moveTo - projectile.Center;
+            float magnitude = Magnitude(move);
+            if (magnitude > speed)
+            {
+                move *= speed / magnitude;
+            }
+
+            move = (projectile.velocity * turnResistance + move) / (turnResistance + 1f);
+            magnitude = Magnitude(move);
+            if (magnitude > speed)
+            {
+                move *= speed / magnitude;
+            }
+
+            projectile.velocity = move;
+        }
+
+        public static float Magnitude(Vector2 mag) // For the Move code above
+        {
+            return (float)Math.Sqrt(mag.X * mag.X + mag.Y * mag.Y);
+        }
+
+        public static void LookByVelocity(this Projectile projectile)
+        {
+            if (projectile.velocity.X > 0)
+            {
+                projectile.spriteDirection = 1;
+                projectile.direction = 1;
+            }
+            else if (projectile.velocity.X < 0)
+            {
+                projectile.spriteDirection = -1;
+                projectile.direction = -1;
+            }
+        }
+    }
+}
