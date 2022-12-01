400c4cbfdb5c4a828f1c5d13c935a9b957fdc1c0
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-11-30 21:37:42 -0500

    fg

---
+++ Content/Items/Weapon/Corruption/Warlock/MonchBow/BoeyrProjectile.cs
@@ -0,0 +1,30 @@
+﻿using Malignant.Common.Projectiles;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Malignant.Content.Items.Weapon.Corruption.Warlock.MonchBow
+{
+    public class BoeyrProjectile : ChargedBowProjectile
+    {
+        public override (Vector2, Vector2) StringTexturePositions => (new Vector2(3, 9), new Vector2(3, 50));
+        public override int ShootProjectileType => ModContent.ProjectileType<BoeyrSkullProjectile>();
+        public override int StringThickness => 1;
+
+        public override int ChargeFramesMax => 70;
+        public override void Charge()
+        {
+            Vector2 rotVector = Projectile.rotation.ToRotationVector2();
+            arrow.Center = StringPoint(0.5f) + rotVector * 34;
+            arrow.velocity = rotVector;
+
+            arrow.netUpdate = true;
+        }
+    }
+}
---
+++ Content/Items/Weapon/Corruption/Warlock/NightsisterBlade/NightsisterBlade.cs
@@ -0,0 +1,53 @@
+using Malignant.Common;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Malignant.Content.Items.Weapon.Corruption.Warlock.NightsisterBlade
+{
+    public class NightsisterBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nightsister's Blade");
+            Tooltip.SetDefault("Summons blades that orbit around you and inflict venom\nForged from the fury of the night.");
+            Item.staff[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 34;
+            Item.width = 66;
+            Item.height = 66;
+            Item.useTime = 20;
+            Item.DamageType = DamageClass.Melee;
+            Item.useAnimation = 20; 
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 6;
+            Item.value = Item.sellPrice(0, 12, 0, 0);
+            Item.rare = ItemRarityID.Pink;
+            Item.UseSound = SoundID.Item117;
+            Item.shoot = ModContent.ProjectileType<NightsisterBladeProjectile>();
+            Item.shootSpeed = 1f;
+            Item.channel = true;
+            Item.autoReuse = true;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+        }
+        
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.ChlorophyteBar, 12)
+                .AddIngredient(ItemID.DarkShard, 5)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+        public override bool CanUseItem(Player player)
+        {
+            
+
+            return true;
+        }
+    }
+}
