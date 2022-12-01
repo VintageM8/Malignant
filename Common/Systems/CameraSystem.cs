29dc44f0d6706e70f1709e9c8284b07a0cf3a8e6
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-11-30 23:07:44 -0500

    Camera System

---
+++ Common/Systems/CameraSystem.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using System;
+using Terraria.ID;
+
+namespace Malignant.Common.Systems
+{
+    public class CameraSystem : ModSystem
+    {
+        public static int ShakeTimer = 0;
+        public static float ScreenShakeAmount = 0;
+
+        public override void ModifyScreenPosition()
+        {
+            Player player = Main.LocalPlayer;           
+            if (!Main.gameMenu)
+            {
+                ShakeTimer++;
+                if (ScreenShakeAmount >= 0 && ShakeTimer >= 5)
+                {
+                    ScreenShakeAmount -= 0.1f;
+                }
+                if (ScreenShakeAmount < 0)
+                {
+                    ScreenShakeAmount = 0;
+                }
+                Main.screenPosition += new Vector2(ScreenShakeAmount * Main.rand.NextFloat(), ScreenShakeAmount * Main.rand.NextFloat());
+            }
+            else
+            {
+                ScreenShakeAmount = 0;
+                ShakeTimer = 0;
+            }
+        }
+    }
+}
