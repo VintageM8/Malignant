40ff2ba7db196ca630e47a578f9930214519657d
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-02-05 12:16:33 -0500

    Add files via upload

---
+++ Content/Items/Misc/HolyGreatsword/HolyGreatsword.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Malignant.Content.Items.Misc.WarbladeofLight;
+using Malignant.Content.Items.Snow.Cocytus.NjorSword;
+using Terraria.DataStructures;
+
+namespace Malignant.Content.Items.Misc.HolyGreatsword
+{
+    public class HolyGreatsword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Holy Greatsword");
+            Tooltip.SetDefault("This sword swords"); //10/10 tooltip
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 16;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 0;
+            Item.height = 0;
+            Item.useTime = 100;
+            Item.useAnimation = 100;
+            Item.useStyle = ItemUseStyleID.Ra;
+            Item.knockBack = 4;
+            Item.value = 10000;
+            Item.noMelee = true;
+
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<HolyGSProj>();
+            Item.shootSpeed = 20f;
+            Item.noUseGraphic = true;
+            Item.value = Item.sellPrice(0, 2, 50, 0);
+            Item.rare = ItemRarityID.LightPurple;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 1);
+
+            return false;
+        }
+    }
+}
