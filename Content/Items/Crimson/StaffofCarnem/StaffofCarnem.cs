d47d6e88905e9b5dd4b02e871c71698f0648564f
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-02-20 02:13:49 -0500

    New content + balance changes^2

---
+++ Content/Items/Crimson/StaffofCarnem/StaffofCarnem.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Malignant.Content.Items.Crimson.StaffofCarnem
+{
+    public class StaffofCarnem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Staff of Carnem");
+            Tooltip.SetDefault("Fires a helix of flesh\nChunks of flesh will splatter out after hitting the wretched");
+            Item.staff[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 30;
+            Item.width = 62;
+            Item.height = 62;
+            Item.DamageType = DamageClass.Magic;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 5f;
+            Item.value = Item.buyPrice(0, 8, 50, 0);
+            Item.rare = ItemRarityID.Yellow;
+            Item.channel = true;
+            Item.autoReuse = true;
+            Item.UseSound = SoundID.Item24;
+            Item.shoot = ModContent.ProjectileType<CarnemProj>();
+            Item.shootSpeed = 10f;
+            Item.mana = 28;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage, knockback, player.whoAmI, 0f, 1f);
+            Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage, knockback, player.whoAmI, 0f, 0f);
+            return false;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+        }
+    }
+}
