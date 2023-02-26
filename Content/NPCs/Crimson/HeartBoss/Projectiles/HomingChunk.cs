88bd18d93cee731cc7628fbb1e0077719f7108a0
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-02-26 18:37:31 -0500

    Updated crimson enemies

---
+++ Content/NPCs/Crimson/HeartBoss/Projectiles/HomingChunk.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace Malignant.Content.NPCs.Crimson.HeartBoss.Projectiles
+{
+    public class HomingChunk : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Homing Chunk");
+            Main.projFrames[Projectile.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.hostile = true;
+            Projectile.penetrate = 1;
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 80;
+        }
+        public override void AI()
+        {
+            if (++Projectile.frameCounter >= 5)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= 2)
+                    Projectile.frame = 0;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f;
+            if (Projectile.localAI[0] == 0)
+            {
+                AdjustMagnitude(ref Projectile.velocity);
+            }
+            Projectile.localAI[0]++;
+            if (Projectile.localAI[0] < 60)
+            {
+                Vector2 move = Vector2.Zero;
+                float distance = 600;
+                bool targetted = false;
+                for (int p = 0; p < Main.maxPlayers; p++)
+                {
+                    Player target = Main.player[p];
+                    if (!target.active || target.dead || target.invis || !Collision.CanHit(Projectile.Center, 0, 0, target.Center, 0, 0))
+                        continue;
+
+                    Vector2 newMove = target.Center - Projectile.Center;
+                    float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+                    if (distanceTo < distance)
+                    {
+                        move = newMove;
+                        distance = distanceTo;
+                        targetted = true;
+                    }
+                }
+
+                if (targetted)
+                {
+                    AdjustMagnitude(ref move);
+                    Projectile.velocity = (10 * Projectile.velocity + move) / 11f;
+                    AdjustMagnitude(ref Projectile.velocity);
+                }
+            }
+        }
+        private static void AdjustMagnitude(ref Vector2 vector)
+        {
+            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude > 6f)
+            {
+                vector *= 12f / magnitude;
+            }
+        }
+        public override void OnHitPlayer(Player target, int damage, bool crit) => Projectile.Kill();
+        public override void Kill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+            for (int i = 0; i < 10; i++)
+            {
+                int dustIndex = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, Scale: 2f);
+                Main.dust[dustIndex].velocity *= 4f;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int dustIndex = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Smoke);
+                Main.dust[dustIndex].velocity *= 4f;
+            }
+        }
+    }
+}
---
+++ Content/NPCs/Crimson/MutilatedFlesh/MutilatedFlesh.cs
@@ -0,0 +1,56 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Malignant.Content.Dusts;
+using Malignant.Content.NPCs.Corruption.CursedOccultist;
+
+namespace Malignant.Content.NPCs.Crimson.MutilatedFlesh
+{
+    public class MutilatedFlesh : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mutilated Flesh");
+            Main.npcFrameCount[NPC.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 26;
+            NPC.height = 24;
+            NPC.damage = 18;
+            NPC.defense = 4;
+            NPC.lifeMax = 100;
+            NPC.HitSound = SoundID.NPCHit13;
+            NPC.DeathSound = SoundID.NPCDeath12;
+            NPC.value = 650f;
+            NPC.knockBackResist = 0.5f;
+            NPC.aiStyle = 67;
+            AIType = 360;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo) => spawnInfo.Player.ZoneCrimson ? 0.50f : 0f;
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            int num = NPC.life > 0 ? 1 : 5;
+            for (int k = 0; k < num; k++)
+            {
+                Dust.NewDust(NPC.position, NPC.width, NPC.height, ModContent.DustType<Blood>());
+            }
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter += 1.0;
+            if (NPC.frameCounter > 8.0)
+            {
+                NPC.frameCounter = 0.0;
+                NPC.frame.Y += frameHeight;
+                if (NPC.frame.Y > frameHeight * 2)
+                {
+                    NPC.frame.Y = 0;
+                }
+            }
+        }
+    }
+}
