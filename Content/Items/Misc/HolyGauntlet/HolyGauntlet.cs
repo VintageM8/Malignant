4948743899a48080401523b080b88be1b3fe6e3d
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-03-06 02:35:34 -0500

    Added Holy Gauntlet

---
+++ Content/Items/Misc/HolyGauntlet/HolyGauntlet.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Malignant.Common.Players;
+
+namespace Malignant.Content.Items.Misc.HolyGauntlet
+{
+    public class HolyGauntlet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Holy Gauntlet");
+            Tooltip.SetDefault("Healing for any health releases a burst of homing energy");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 32;
+            Item.value = Item.buyPrice(0, 5, 0, 0);
+            Item.rare = ItemRarityID.Expert;
+            Item.accessory = true;
+            Item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<MalignantPlayer>().HolyGauntlet = true;
+        }
+    }
+}
