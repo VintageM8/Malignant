5cd53348f7b74ef3306fd8d55e257443882bab97
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-03-06 00:38:43 -0500

    Mod should build

---
+++ Content/Items/Hell/MarsHell/MarsHell.cs
@@ -0,0 +1,68 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using static Terraria.ModLoader.ModContent;
+using Malignant.Common.Players;
+
+namespace Malignant.Content.Items.Hell.MarsHell
+{
+    public class MarsHell : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Mars Hell");
+            Tooltip.SetDefault("Shoots out a gernade\n" +
+                "Gernade gets stronger overtime");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 38;
+            Item.crit = 0;
+            Item.damage = 23;
+            Item.useAnimation = 50;
+            Item.useTime = 50;
+            Item.noMelee = true;
+            Item.autoReuse = true;
+            //Item.useAmmo = AmmoID.Bullet;
+            Item.DamageType = DamageClass.Ranged;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.Item36;
+            Item.rare = ItemRarityID.Yellow;
+            Item.shootSpeed = 10f;
+            Item.shoot = ProjectileID.Bullet;
+            Item.channel = true;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (player.GetModPlayer<MalignantPlayer>().itemCombo >= 0)
+            {
+                type = ModContent.ProjectileType<Gernade1>();
+                damage = 23;
+                Item.useTime = 50;
+            }
+            if (player.GetModPlayer<MalignantPlayer>().itemCombo >= 10)
+            {
+                type = ModContent.ProjectileType<Gernade2>();
+                damage = 36;
+                Item.useTime = 38;
+            }
+            if (player.GetModPlayer<MalignantPlayer>().itemCombo >= 10)
+            {
+                type = ModContent.ProjectileType<Gernade3>();
+                damage = 48;
+                Item.crit = 4;
+            }
+
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-7, 0);
+        }
+    }
+}
