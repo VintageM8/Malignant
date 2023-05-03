b0d2c681967c088bc2c2c0a2a229e4192db00c0c
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2023-05-03 04:49:51 -0400

    flame

--- Content/StarParticle.cs
+++ Content/StarParticle.cs
@@ -1,42 +1,52 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ParticleLibrary;
 using Terraria;
-using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+using Malignant.Core;
 
 namespace Malignant.Content
 {
-    public class StarParticle : Particle
+    public class StarParticle : Particle //Finna rename this bitch
     {
-        public override string Texture => "Divergency/Assets/Textures/ParticleTextures/SmallStar";
 
+        private int frameCount;
+        private int frameTick;
+        public override string Texture => "Malignant/Assets/Textures/Pixel";
         public override void SetDefaults()
         {
-            width = 1;
-            height = 1;
-            timeLeft = 30;
-
+            width = 34;
+            height = 34;
+            Scale = 1f;
+            timeLeft = 40;
         }
+
 
         public override void AI()
         {
-            velocity *= 0.96f;
+            rotation = velocity.ToRotation();
 
-            Scale /= 1.1f;
+            velocity *= 0.98f;
+            Scale *= 1.05f;
+            if (Scale <= 0f)
+                active = false;
+            opacity = 125f;
         }
 
-        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 drawPos, Color lightColor)
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color lightColor)
         {
+            Texture2D meow = Request<Texture2D>("Malignant/Assets/Textures/ParticleTextures/Particle1").Value;
+            Texture2D weed = Request<Texture2D>("Malignant/Assets/Textures/ParticleTextures/Particle1").Value;
+            Texture2D Fmrnch = Request<Texture2D>("Malignant/Assets/Textures/ParticleTextures/Particle1").Value; //I hate the French
 
-            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+            float alpha = timeLeft <= 20 ? 1f - 1f / 20f * (20 - timeLeft) : 1f;
+            if (alpha < 0f) alpha = 0f;
+            Color color = Color.Multiply(new(2.58f, 1.39f, 0.95f, 0), alpha / 2);
+            Color color2 = Color.Multiply(new(2.58f, 1.39f, 0.95f, 0), alpha / 5);
 
-            float alpha = timeLeft > 40f ? (20f - (timeLeft - 40f)) / 20f : timeLeft <= 20f ? timeLeft / 20f : 1f;
-            Color color2 = Color.Multiply(new(2.55f, 2.55f, 2.55f, 0), alpha);
-
-            spriteBatch.Draw(texture, Center - Main.screenPosition, texture.Bounds, color * alpha, rotation, texture.Size() * 0.5f, Scale, SpriteEffects.None, 0f);
-            spriteBatch.Draw(texture, Center - Main.screenPosition, texture.Bounds, color2, rotation, texture.Size() * 0.5f, Scale / 2f, SpriteEffects.None, 0f);
-
-
+            spriteBatch.Draw(meow, position - Main.screenPosition, meow.AnimationFrame(ref frameCount, ref frameTick, 7, 7, true), color2, 0f, new Vector2(meow.Width / 2f, meow.Height / 2f / 7f), Scale / 3.2f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(Fmrnch, position - Main.screenPosition, new Rectangle(0, 0, Fmrnch.Width, Fmrnch.Height), color, rotation, new Vector2(Fmrnch.Width / 2f, Fmrnch.Height / 2f), 0.17f * Scale, SpriteEffects.None, 0f);
             return false;
         }
     }
